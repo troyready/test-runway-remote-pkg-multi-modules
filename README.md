d175ce2fe84c39c7e75a28fbbabbd5ff4b6c2814
troyready <troy@troyready.com>
2018-06-12 23:52:50 -0700

    v1

---
+++ README.md
@@ -0,0 +1,3 @@
+## Test Runway Module for Multi-Version Package References
+
+This simple repo is for integration testing of Runway's use of remote CFN packages at different versions in different modules. Git tag `v1.0.0` will produce a noticeably different stack than `v2.0.0`.
---
+++ test_runway_multi_module_blueprints/policy.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+"""Module with test IAM policy."""
+
+from troposphere import Join, iam
+
+import awacs.ec2
+from awacs.aws import Allow, Policy, Statement
+
+from stacker.blueprints.base import Blueprint
+from stacker.blueprints.variables.types import CFNString
+
+
+class IamPolicy(Blueprint):
+    """Stacker blueprint for test iam policy."""
+
+    VARIABLES = {
+        'PolicySuffix': {'type': CFNString,
+                         'description': 'Text to append to policy name'}
+    }
+
+    def create_template(self):
+        """Create template (main function called by Stacker)."""
+        template = self.template
+        variables = self.get_variables()
+        self.template.add_version('2010-09-09')
+        self.template.add_description("Test Policy template v1")
+
+        template.add_resource(
+            iam.ManagedPolicy(
+                'ManagedPolicy',
+                Description='Sample managed policy.',
+                ManagedPolicyName=Join(
+                    '-',
+                    ['testpolicyv1',
+                     variables['PolicySuffix'].ref]
+                ),
+                Path='/',
+                PolicyDocument=Policy(
+                    Version='2012-10-17',
+                    Statement=[
+                        Statement(
+                            Action=[awacs.ec2.DescribeInstances,
+                                    awacs.ec2.DescribeTags,
+                                    awacs.ec2.CreateTags],
+                            Effect=Allow,
+                            Resource=['*']
+                        )
+                    ]
+                )
+            )
+        )
+
+
+# Helper section to enable easy blueprint -> template generation
+# (just run `python <thisfile>` to output the json)
+if __name__ == "__main__":
+    from stacker.context import Context
+    print(IamPolicy('test',
+                    Context({"namespace": "test"}),
+                    None).to_json())
